cb5b507cd7ebc03cb0f1741a0d8879d9e4cc6de2
TanmayLodha <54031403+TanmayLodha@users.noreply.github.com>
2025-07-01 02:24:39 +0530

    added old template

---
+++ templates/yo.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MAXN 100000
+#define long_max 9223372036854775807
+const ll mod = 7 + 1e9;
+
+ll gcd(ll a, ll b)
+{
+  if (b == 0)
+    return a;
+  return gcd(b, a % b);
+}
+
+
+ll lcm(ll a, ll b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+ll fact(ll n)
+{
+	if(n==0)
+		return 1;
+    ll res = 1;
+    for (ll i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
+
+ll nCr(ll n, ll r)
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+
+bool compare(ll& x, ll& y) {
+	// if(x.first == y.first) return x.second > y.second;
+	return x > y;
+}
+
+void solve() {
+    
+} 
+
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	// generate();
+    ll t=1;
+    // cin>>t;
+    while(t--) {
+    	solve();
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
