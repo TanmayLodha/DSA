c3ef78af40cd76bbe60113e37178d2c454cb2383
TanmayLodha <54031403+TanmayLodha@users.noreply.github.com>
2025-07-05 21:11:54 +0530

    template file formatting and fixes

--- templates/yo.cpp
+++ templates/yo.cpp
@@ -5,59 +5,47 @@ using namespace std;
 #define long_max 9223372036854775807
 const ll mod = 7 + 1e9;
 
-ll gcd(ll a, ll b)
-{
-  if (b == 0)
-    return a;
-  return gcd(b, a % b);
+ll gcd(const ll a, const ll b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
 }
 
 
-ll lcm(ll a, ll b)
-{
+ll lcm(const ll a, const ll b) {
     return (a / gcd(a, b)) * b;
 }
 
-ll fact(ll n)
-{
-	if(n==0)
-		return 1;
+ll fact(const ll n) {
+    if (n == 0)
+        return 1;
     ll res = 1;
     for (ll i = 2; i <= n; i++)
         res = res * i;
     return res;
 }
 
-ll nCr(ll n, ll r)
-{
+ll nCr(const ll n, const ll r) {
     return fact(n) / (fact(r) * fact(n - r));
 }
 
-bool compare(ll& x, ll& y) {
-	// if(x.first == y.first) return x.second > y.second;
-	return x > y;
+bool compare(const ll &x, const ll &y) {
+    // if(x.first == y.first) return x.second > y.second;
+    return x > y;
 }
 
 void solve() {
-    
-} 
-
-
-int main(){
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	// generate();
-    ll t=1;
-    // cin>>t;
-    while(t--) {
-    	solve();
-    }
-    
-    return 0;
 }
 
 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll t = 1;
+    // cin>>t;
+    while (t--) {
+        solve();
+    }
 
-
-
-
+    return 0;
+}
